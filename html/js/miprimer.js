51000bf1b58f1274756d7738ef6b11c3a82cde34
erikcn47 <ecorralizan01@educarex.es>
2026-04-21 11:19:39 +0200

    add js folder

---
+++ html/js/miprimer.js
@@ -0,0 +1,10 @@
+//mi primer archivo js
+myName="Erik";
+console.log(myName);
+console.log(typeof(myName));
+edad=7;
+console.log(typeof(edad));
+edad=String(edad);
+console.log("He cambiado el tipo de la variable");
+console.log(edad);
+console.log(typeof(edad));
