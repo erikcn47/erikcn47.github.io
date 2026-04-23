011474297e0c419c59684d133ca99250b264f285
erikcn47 <ecorralizan01@educarex.es>
2026-04-23 14:25:29 +0200

    add codekata and funciones autoinvocadas

--- html/js/codekata.js
+++ html/js/codekata.js
@@ -1,3 +1,10 @@
-function getCopyright(name, year) {
-    
+let result = function(name, year, formatter) {
+    let copyright = formatter(name, year);
+    return copyright;
+};
+
+let formatter = function(name, year) {
+    return name + " | " + year;
 }
+
+console.log(result("Erik", 2026, formatter));
--- html/js/miprimer.js
+++ html/js/miprimer.js
@@ -1,5 +1,5 @@
 //mi primer archivo js
-let myName="Erik";
+/*let myName="Erik";
 console.log(myName);
 console.log(typeof(myName));
 let edad=7;
@@ -8,4 +8,9 @@ let edadString=String(edad); // realizo la operación de Casting
 console.log("He cambiado el tipo de la variable");
 console.log(edadString);
 console.log(typeof(edadString));
-const PI = 3.1416;
+const PI = 3.1416;*/
+
+//FUNCIONES AUTOINVOCADAS
+(function(a, b = 2, c) {
+    console.log(a + b + c);
+})(5, undefined, 7);
