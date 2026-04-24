7663869e3425e318f29b82f41f3c8ab25cffce02
erikcn47 <ecorralizan01@educarex.es>
2026-04-24 14:11:37 +0200

    add coments

--- html/js/codekata.js
+++ html/js/codekata.js
@@ -1,10 +1,15 @@
+//Creo una función llamada 'result' que recibe un nombre, un año y una función de formato.
+// Dentro de 'result', uso la función de formato para crear un copyright y lo devuelvo.
 let result = function(name, year, formatter) {
+    // Llamo a la función de formato con el nombre y el año para obtener el copyright.
     let copyright = formatter(name, year);
     return copyright;
 };
 
+// Ahora defino la función 'formatter' que junta el nombre y el año con una barra que los separa.
 let formatter = function(name, year) {
     return name + " | " + year;
 }
 
+//Ejecuto la función con los datos que yo quiero y el formato que he definido, y luego imprimo el resultado en la consola.
 console.log(result("Erik", 2026, formatter));
--- html/js/miprimer.js
+++ html/js/miprimer.js
@@ -11,6 +11,8 @@ console.log(typeof(edadString));
 const PI = 3.1416;*/
 
 //FUNCIONES AUTOINVOCADAS
+//Esta función coge tres parametros y los suma, hacemos también que el segundo parámetro tenga un valor por defecto de 2, si no se le asigna ningún valor a ese parámetro, se le asignará el valor por defecto
 (function(a, b = 2, c) {
     console.log(a + b + c);
 })(5, undefined, 7);
+//Aquí lo que hacemos es llamar a la función inmediatamente después de definirla, pasándole los argumentos necesarios para que se ejecute correctamente. En este caso, el resultado de la suma será 14 (5 + 2 + 7).
