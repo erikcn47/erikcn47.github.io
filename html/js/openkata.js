59ff6ccc80af901839161572a21bd85e5c91279f
erikcn47 <ecorralizan01@educarex.es>
2026-04-29 11:27:22 +0200

    add openkata.js

---
+++ html/js/openkata.js
@@ -0,0 +1,14 @@
+/*let hasPizza = true;
+
+if (hasPizza == false) {
+    console.log("No tengo pizza");
+} else if (hasPizza == true) {
+    console.log("Tengo pizza");
+} else {
+    console.log(hasPizza);
+}*/
+let year = false;
+
+if (year != true) {
+    console.log("Ok");
+}
