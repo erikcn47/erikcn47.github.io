2e1b583b693acd39b055eb5a2e73e94fc30d5a20
erikcn47 <ecorralizan01@educarex.es>
2026-04-30 10:15:32 +0200

    delete openkata

--- html/js/codekata.js
+++ html/js/codekata.js
@@ -1,6 +1,6 @@
 //Creo una función llamada 'result' que recibe un nombre, un año y una función de formato.
 // Dentro de 'result', uso la función de formato para crear un copyright y lo devuelvo.
-let result = function(name, year, formatter) {
+/*let result = function(name, year, formatter) {
     // Llamo a la función de formato con el nombre y el año para obtener el copyright.
     let copyright = formatter(name, year);
     return copyright;
@@ -12,4 +12,49 @@ let formatter = function(name, year) {
 }
 
 //Ejecuto la función con los datos que yo quiero y el formato que he definido, y luego imprimo el resultado en la consola.
-console.log(result("Erik", 2026, formatter));
+console.log(result("Erik", 2026, formatter));*/
+/*let hasPizza = true;
+
+if (hasPizza == false) {
+    console.log("No tengo pizza");
+} else if (hasPizza == true) {
+    console.log("Tengo pizza");
+} else {
+    console.log(hasPizza);
+}*/
+/*let year = false;
+
+if (year != true) {
+    console.log("Ok");
+}*/
+/*let year = 2023;
+
+if (year == 2023) {
+    console.log("Ok");
+}*/
+
+/*let company = "OpenWebinars";
+const year = 2023;
+
+function getAcademyInfo() {
+    let format = "From" + company;
+
+    if (!year) {
+        let format = "Make in" + company;
+        return format;
+    }
+    
+    return format + " in " + year;
+}*/
+
+function printName() {
+    let name = "Erik";
+    console.log(name);
+
+    if (name === "Erik") {
+        let surname = "Corraliza";
+        console.log(surname);
+    }
+}
+
+printName();
